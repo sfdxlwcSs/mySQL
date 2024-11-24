f45ec0fb6f3ed11aacc137c8d874a7c8f4461e2a
Somnath Sharma <52417989+sfdxlwcSs@users.noreply.github.com>
2024-11-24 16:51:07 +0530

    sync

--- 24thNovember.sql
+++ 24thNovember.sql
@@ -1,8 +1,17 @@
 # pivot table
 # THEN 1 means 
+
+INSERT INTO session5.transaction (Trans_ID, Acct_Num, Tran_Amount, Tran_Date,Channel)
+VALUES 
+    (21,1001, 200, '2020-04-24', 'UPI transfer'),
+   (22,1002, 200, '2020-05-24', 'UPI transfer'),
+     (23,1001, 200, '2020-05-24', 'Bankers cheque');
+
 SELECT Month(T.Tran_Date) as "Month",  
 count(case when T.Channel= 'ATM withdrawal' THEN 1 END) ATM_transaction, 
 count(case when T.Channel = 'Online transfer' THEN 1 END) Online_transfer,  
-count(case when T.Channel = 'Branch deposit' THEN 1 END) Branch_deposit
+count(case when T.Channel = 'Branch deposit' THEN 1 END) Branch_deposit,
+count(case when T.Channel = 'UPI transfer' THEN 1 END) UPI_transfer,
+count(case when T.Channel = 'Bankers cheque' THEN 1 END) Bankers_cheque
  FROM Transaction T 
  GROUP BY month(T.Tran_Date);
--- Session 6-20241123T185113Z-001/Session 6/pivot_recursive_examples.sql
+++ Session 6-20241123T185113Z-001/Session 6/pivot_recursive_examples.sql
@@ -15,6 +15,8 @@ INSERT INTO sales VALUES ('ProductB', 'East', 90);
 INSERT INTO sales VALUES ('ProductA', 'West', 130);
 INSERT INTO sales VALUES ('ProductB', 'West', 110);
 select * from sales;
+
+# you are not creating a column call amount_sold where result in store and displayed as North or soth as the case may be
 SELECT
     product,
     SUM(CASE WHEN region = 'North' THEN amount_sold END) AS North,
@@ -46,6 +48,15 @@ INSERT INTO CUSTOMER_HOUSEHOLD VALUES
 (123005, 'ROBERT' , 'SARAH') ,
 (123006, 'ANDY' , 'SARAH') ;
 select * from CUSTOMER_HOUSEHOLD;
+
+# The CTE consists of two parts:
+# Anchor Query (Base Case):
+SELECT Name, Parent_Name, CAST(Name AS CHAR(200))
+FROM CUSTOMER_HOUSEHOLD
+WHERE Parent_Name IS NULL;
+
+# Recursive Query:
+
 WITH RECURSIVE Family ( NAME, PARENT_NAME, Hierarchy) AS
 (SELECT Name, Parent_name, CAST(Name AS CHAR(200))
 FROM CUSTOMER_HOUSEHOLD
@@ -57,6 +68,8 @@ JOIN CUSTOMER_HOUSEHOLD ch
 ON cf.name = ch.parent_name )
 SELECT * FROM Family ORDER BY Hierarchy;
 
+
+
 WITH RECURSIVE countup AS (
   SELECT 1 AS n
   UNION ALL
@@ -64,11 +77,32 @@ WITH RECURSIVE countup AS (
 )
 SELECT * FROM countup;
 
+WITH RECURSIVE countup AS (
+  SELECT 1 AS n
+  UNION ALL
+  SELECT n + 1 FROM countup WHERE n < 5
+)
+SELECT * FROM countup;
+# even number
+WITH RECURSIVE countup AS (
+  SELECT 2 AS n
+  UNION ALL
+  SELECT n + 2 FROM countup WHERE n < 20
+)
+SELECT * FROM countup;
+# odd number
+WITH RECURSIVE countup AS (
+  SELECT 1 AS n
+  UNION ALL
+  SELECT n + 2 FROM countup WHERE n < 20
+)
+SELECT * FROM countup;
+
 -- Recursive CTE for powers of 2
 WITH RECURSIVE powers_of_2 AS (
   SELECT 1 AS power
   UNION ALL
-  SELECT power * 2 FROM powers_of_2 WHERE power < 16
+  SELECT power * 2 FROM powers_of_2 WHERE power < 256
 )
 SELECT power FROM powers_of_2;
 
@@ -76,7 +110,7 @@ SELECT power FROM powers_of_2;
 WITH RECURSIVE fibonacci AS (
   SELECT 0 AS n, 1 AS next
   UNION ALL
-  SELECT next, n + next FROM fibonacci WHERE n < 6
+  SELECT next, n + next FROM fibonacci WHERE n < 14
 )
 SELECT n FROM fibonacci;
 
