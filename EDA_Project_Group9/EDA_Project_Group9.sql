85c5f78bdbcfef1026a506dae0d26321c4c222aa
Somnath Sharma <52417989+sfdxlwcSs@users.noreply.github.com>
2025-03-18 20:47:53 +0530

    project

---
+++ EDA_Project_Group9/EDA_Project_Group9.sql
@@ -0,0 +1,64 @@
+create Database Group9;  -- create Database
+
+Use  Group9; 
+
+CREATE TABLE StudentDetails (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    Gender ENUM('Male', 'Female', 'Other'),
+    Address VARCHAR(255),
+    SocioEconomicStatus ENUM('Low', 'Middle', 'High'),
+    ScholarshipStatus ENUM('Yes', 'No'),
+    Nationality VARCHAR(100)
+);
+
+CREATE TABLE PlacementData (
+    StudentID INT PRIMARY KEY,
+    CGPA DECIMAL(3,2),
+    Internships INT,
+    Projects INT,
+    Workshops_Certifications INT,
+    AptitudeTestScore DECIMAL(5,2),
+    SoftSkillsRating DECIMAL(3,2),
+    ExtracurricularActivities ENUM('Yes', 'No'),
+    PlacementTraining ENUM('Yes', 'No'),
+    SSC_Marks DECIMAL(5,2),
+    HSC_Marks DECIMAL(5,2),
+    PlacementStatus ENUM('Placed', 'Not Placed'),
+    FOREIGN KEY (StudentID) REFERENCES StudentDetails(StudentID)
+);
+
+CREATE TABLE CompanyDetails (
+    CompanyID INT PRIMARY KEY,
+    CompanyName VARCHAR(100),
+    IndustryType VARCHAR(100),
+    MinCGPARequirement DECIMAL(3,2),
+    HiringStatus ENUM('Active', 'Inactive')
+);
+
+CREATE TABLE JobOffers (
+    OfferID INT PRIMARY KEY,
+    StudentID INT,
+    CompanyID INT,
+    SalaryPackage INT,
+    JobRole VARCHAR(100),
+    OfferStatus ENUM('Accepted', 'Rejected', 'Pending'),
+    FOREIGN KEY (StudentID) REFERENCES StudentDetails(StudentID),
+    FOREIGN KEY (CompanyID) REFERENCES CompanyDetails(CompanyID)
+);
+
+ALTER TABLE PlacementData  
+MODIFY StudentID INT NOT NULL;
+-- Ensure Every Student Must Have a Placement Record
+
+ALTER TABLE PlacementData  
+ADD CONSTRAINT fk_student FOREIGN KEY (StudentID)  
+REFERENCES StudentDetails(StudentID)  
+ON DELETE CASCADE;  -- Deletes placement record if student is removed
+
+Describe studentDetails;
+SHOW COLUMNS FROM placementData;
+
+
+
+
